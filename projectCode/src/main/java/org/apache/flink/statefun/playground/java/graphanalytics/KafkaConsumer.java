82391e0b60e23c006b4ff2a00cb2e7431cd6fadf
toucham <thachathumam@gmail.com>
2022-04-18 12:25:04 -0400

    delete .vscode settings

--- projectCode/src/main/java/org/apache/flink/statefun/playground/java/graphanalytics/KafkaConsumer.java
+++ projectCode/src/main/java/org/apache/flink/statefun/playground/java/graphanalytics/KafkaConsumer.java
@@ -4,17 +4,20 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
- * KafkaApp is an application for running Kafka consumer to retrieve topics/requests from Kafka.
+ * KafkaConsumer is a data source that will retrieve topics/requests from Kafka.
  * This is done so that we don't lose any message and the message will be sent in order.
+ * 
+ * Not tested yet
  */
 public class KafkaConsumer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         
-        String bootstrapServer = "0.0.0.0";
+        String bootstrapServer = "localhost:9092";
         KafkaSource<String> source = KafkaSource.<String>builder()
             .setBootstrapServers(bootstrapServer)
             .setTopics("quickstart")
@@ -23,7 +26,10 @@ public class KafkaConsumer {
             .setValueOnlyDeserializer(new SimpleStringSchema())
             .build();
 
-        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+        DataStream<String> kafkaSource = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+        kafkaSource.print();
+
+        env.execute("Consume events from Kafka");
     }
     
 }
